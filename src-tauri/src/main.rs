6fc4497d96d302279fda2b02f9d2887dae760911
Googlefan256 <googlefan@neody.dev>
2024-09-11 20:56:16 +0900

    feat: sbv2file

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 mod tts;
-
 use hf_hub::api::sync::Api;
+
+#[tauri::command]
+fn open() {
+    open::that(format!(
+        "C:/Users/{}/AppData/Local/sbv2-gui/models",
+        whoami::username()
+    ))
+    .ok();
+}
 
 fn main() {
     ort::init_from(
@@ -20,7 +28,8 @@ fn main() {
         .invoke_handler(tauri::generate_handler![
             tts::reload_models,
             tts::synthesize,
-            tts::models
+            tts::models,
+            open,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
